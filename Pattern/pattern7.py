c1ee393cb5042f1c4b2ae9c0c4dc04f86eb8dbd5
vinit <vinitsonawane76@gmail.com>
2026-08-19 11:47:20 +0530

    Pattern7

---
+++ Pattern/pattern7.py
@@ -0,0 +1,11 @@
+def pattern7(n):
+    for i in range(n):
+
+        for j in range(n-i-1):
+            print(" ",end="")
+        for j in range(2*i+1):
+            print("*",end="")
+        for j in range(n-i-1):
+            print(" ",end="")
+        print()
+pattern7(5)
